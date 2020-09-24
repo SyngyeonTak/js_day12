b90998214dacbc66dd730898cc3a05801682cf3e
SyngyeonTak <67184044+SyngyeonTak@users.noreply.github.com>
2020-09-24 23:39:53 +0900

    c

---
+++ data.js
@@ -0,0 +1,15 @@
+var data = [
+    ["../img/nation/1.png", "세계 최대 강대국", "미국"],
+    ["../img/nation/2.jpg", "파파리치", "말레이시아"],
+    ["../img/nation/3.jpg", "신화", "그리스"],
+    ["../img/nation/4.png", "신사의 나라, 007", "영국"],
+    ["../img/nation/5.jpg", "나찌", "독일"],
+    ["../img/nation/6.png", "무적함대", "스페인"],
+    ["../img/nation/7.jpg", "에펠탑", "프랑스"],
+    ["../img/nation/8.gif", "차이코프스키, 백조의 호수", "러시아"],
+    ["../img/nation/9.png", "쌈바축구", "브라질"],
+    ["../img/nation/10.jpg", "아이스하키", "캐나다"],
+    ["../img/nation/11.jpg", "후지산 원숭이", "일본"],
+    ["../img/nation/12.png", "쿵푸 팬더", "중국"],
+    ["../img/nation/13.gif", "코알라, 캥거루", "호주"]
+];
